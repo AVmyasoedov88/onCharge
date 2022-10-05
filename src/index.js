78ca809af1cc846e937484d670ae84aa8355b538
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2022-10-05 21:50:04 +0300

    5/1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-import onChange from 'on-change'
+import app from './mvc.js';
 
-
-console.log('Herny')
-
-function app() {
-	const state = {
-		value: 0,
-	};
-
-	const result = document.getElementById('result');
-
-	const watchedState = onChange(
-		state,
-		(path, value) => {
-			console.log(path)
-			result.textContent = value;
-		}
-	);
-
-	const incHandler = () => {
-		watchedState.value += 1;
-	};
-
-	const decHandler = () => {
-		watchedState.value -= 1;
-	};
-
-	const inc = document.getElementById('increment');
-	inc.addEventListener('click', incHandler);
-
-	const dec = document.getElementById('decrement');
-	dec.addEventListener('click', decHandler);
-}
-  
-  app();
+app();
---
+++ src/mvc.js
@@ -0,0 +1,35 @@
+import onChange from 'on-change';
+
+//console.log('HI')
+
+export default function app() {
+	const state = {
+		value: 0,
+	};
+
+	const result = document.getElementById('result');
+
+	const watchedState = onChange(
+		state,
+		(path, value) => {
+			console.log(path)
+			result.textContent = value;
+		}
+	);
+
+	const incHandler = () => {
+		watchedState.value += 1;
+	};
+
+	const decHandler = () => {
+		watchedState.value -= 1;
+	};
+
+	const inc = document.getElementById('increment');
+	inc.addEventListener('click', incHandler);
+
+	const dec = document.getElementById('decrement');
+	dec.addEventListener('click', decHandler);
+}
+  
+  
